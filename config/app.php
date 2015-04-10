25d4aa384631b2bea5a0e66940c5941a239c6856
butschster <butschster@gmail.com>
2015-04-10 13:53:34 +0300

    Инициализация алиасов и сервисных провайдеров вынесена  из конфига в модуль CMS

--- config/app.php
+++ config/app.php
@@ -107,44 +107,12 @@ return [
 	| this array to grant expanded functionality to your applications.
 	|
 	*/
-
-	'providers' => [
+	'providers' => array_merge(include(base_path('/modules/CMS/providers.php')), [
 		/*
-		 * Laravel Framework Service Providers...
+		 * App Service Providers must be here...
 		 */
-		'Illuminate\Foundation\Providers\ArtisanServiceProvider',
-		'Illuminate\Auth\AuthServiceProvider',
-		'Illuminate\Bus\BusServiceProvider',
-		'Illuminate\Cache\CacheServiceProvider',
-		'Illuminate\Foundation\Providers\ConsoleSupportServiceProvider',
-		'Illuminate\Routing\ControllerServiceProvider',
-		'Illuminate\Cookie\CookieServiceProvider',
-		'Illuminate\Database\DatabaseServiceProvider',
-		'Illuminate\Encryption\EncryptionServiceProvider',
-		'Illuminate\Filesystem\FilesystemServiceProvider',
-		'Illuminate\Foundation\Providers\FoundationServiceProvider',
-		'Illuminate\Hashing\HashServiceProvider',
-		'Illuminate\Mail\MailServiceProvider',
-		'Illuminate\Pagination\PaginationServiceProvider',
-		'Illuminate\Pipeline\PipelineServiceProvider',
-		'Illuminate\Queue\QueueServiceProvider',
-		'Illuminate\Redis\RedisServiceProvider',
-		'Illuminate\Auth\Passwords\PasswordResetServiceProvider',
-		'Illuminate\Session\SessionServiceProvider',
-		'Illuminate\Translation\TranslationServiceProvider',
-		'Illuminate\Validation\ValidationServiceProvider',
-		'Illuminate\View\ViewServiceProvider',
-		'Illuminate\Html\HtmlServiceProvider',
-
 		'App\Providers\AppServiceProvider',
-
-		'KodiCMS\CMS\Providers\ModuleServiceProvider',
-		'KodiCMS\CMS\Providers\RouteServiceProvider',
-		'KodiCMS\CMS\Providers\ConfigServiceProvider',
-		'KodiCMS\CMS\Providers\EventServiceProvider',
-		'KodiCMS\CMS\Providers\BusServiceProvider',
-		'KodiCMS\CMS\Providers\AppServiceProvider',
-	],
+	]),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -157,45 +125,9 @@ return [
 	|
 	*/
 
-	'aliases' => [
-		'App'       => 'Illuminate\Support\Facades\App',
-		'Artisan'   => 'Illuminate\Support\Facades\Artisan',
-		'Auth'      => 'Illuminate\Support\Facades\Auth',
-		'Blade'     => 'Illuminate\Support\Facades\Blade',
-		'Bus'       => 'Illuminate\Support\Facades\Bus',
-		'Cache'     => 'Illuminate\Support\Facades\Cache',
-		'Config'    => 'Illuminate\Support\Facades\Config',
-		'Cookie'    => 'Illuminate\Support\Facades\Cookie',
-		'Crypt'     => 'Illuminate\Support\Facades\Crypt',
-		'DB'        => 'Illuminate\Support\Facades\DB',
-		'Eloquent'  => 'Illuminate\Database\Eloquent\Model',
-		'Event'     => 'Illuminate\Support\Facades\Event',
-		'File'      => 'Illuminate\Support\Facades\File',
-		'Hash'      => 'Illuminate\Support\Facades\Hash',
-		'Input'     => 'Illuminate\Support\Facades\Input',
-		'Inspiring' => 'Illuminate\Foundation\Inspiring',
-		'Lang'      => 'Illuminate\Support\Facades\Lang',
-		'Log'       => 'Illuminate\Support\Facades\Log',
-		'Mail'      => 'Illuminate\Support\Facades\Mail',
-		'Password'  => 'Illuminate\Support\Facades\Password',
-		'Queue'     => 'Illuminate\Support\Facades\Queue',
-		'Redirect'  => 'Illuminate\Support\Facades\Redirect',
-		'Redis'     => 'Illuminate\Support\Facades\Redis',
-		'Request'   => 'Illuminate\Support\Facades\Request',
-		'Response'  => 'Illuminate\Support\Facades\Response',
-		'Route'     => 'Illuminate\Support\Facades\Route',
-		'Schema'    => 'Illuminate\Support\Facades\Schema',
-		'Session'   => 'Illuminate\Support\Facades\Session',
-		'Storage'   => 'Illuminate\Support\Facades\Storage',
-		'URL'       => 'Illuminate\Support\Facades\URL',
-		'Validator' => 'Illuminate\Support\Facades\Validator',
-		'View'      => 'Illuminate\Support\Facades\View',
-		'Form'		=> 'Illuminate\Html\FormFacade',
-		'HTML'		=> 'Illuminate\Html\HtmlFacade',
-
-		'UI'		=> 'KodiCMS\CMS\Helpers\UI',
-		'ACL'		=> 'KodiCMS\Users\ACL',
-		'CMS'		=> 'KodiCMS\CMS\Core',
-		'Assets'	=> 'KodiCMS\CMS\Assets\Core',
-	],
+	'aliases' => array_merge(include(base_path('/modules/CMS/aliases.php')), [
+		/*
+		 * App aliases must be here...
+		 */
+	]),
 ];
