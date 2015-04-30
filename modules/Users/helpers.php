34200120940c359308551141f2211dde085c3d9c
Butschster <butschster@gmail.com>
2015-04-30 16:29:12 +0300

    Add phpDoc

--- modules/Users/helpers.php
+++ modules/Users/helpers.php
@@ -2,6 +2,11 @@
 
 use KodiCMS\Users\Model\User;
 
+/**
+ * @param string|array $action
+ * @param User $user
+ * @return bool
+ */
 function acl_check($action, User $user = NULL)
 {
 	return \KodiCMS\Users\ACL::check($action, $user);
