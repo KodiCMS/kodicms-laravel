dc926102b21ec727824b332180a1a441b4bbb099
Butschster <butschster@gmail.com>
2015-05-14 21:38:43 +0300

    fix

--- modules/Dashboard/resources/views/widgets/cache_button/template.blade.php
+++ modules/Dashboard/resources/views/widgets/cache_button/template.blade.php
@@ -1,11 +1,11 @@
 <div class="panel dashboard-widget panel-warning panel-body-colorful panel-dark" data-id="{{ $widget->getId() }}">
-	<button type="button" class="close remove_widget">{{ UI::icon('times') }}</button>
+	<button type="button" class="close remove_widget">{!! UI::icon('times') !!}</button>
 	<div class="panel-body text-center handle">
-		<?php echo Form::button(__('Clear cache'), [
+		{!! Form::button(__('Clear cache'), [
 				'data-icon' => 'trash-o fa-lg',
 				'class' => 'btn btn-lg btn-success btn-flat btn-block',
 				'data-api-url' => '/api.cache.clear',
 				'data-method' => 'DELETE'
-		]); ?>
+		]) !!}
 	</div>
 </div>
