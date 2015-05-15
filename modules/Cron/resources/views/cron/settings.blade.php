2564157ed0b5effc585dbebc38105313ddb3ad72
butschster <butschster@gmail.com>
2015-05-15 18:46:51 +0300

    Исправлено отображение уведомления в настройках задач

--- modules/Cron/resources/views/cron/settings.blade.php
+++ modules/Cron/resources/views/cron/settings.blade.php
@@ -1,13 +1,12 @@
 <div class="panel-heading panel-toggler" data-icon="bolt">
 	<span class="panel-title">@lang('cron::core.settings.title')</span>
 </div>
+<div class="note note-warning">
+	{!! UI::icon('lightbulb-o fa-lg') !!} @lang('cron::core.settings.info')
+	<br /><br />
+	<strong>* * * * * php {{ base_path('artisan') }} cms:cron:run &gt; /dev/null 2&gt;&amp;1</strong>
+</div>
 <div class="panel-body panel-spoiler">
-	<div class="note note-warning">
-		{!! UI::icon('lightbulb-o fa-lg') !!} @lang('cron::core.settings.info')
-		<br /><br />
-		<strong>* * * * * php {{ base_path('artisan') }} cms:cron:run &gt; /dev/null 2&gt;&amp;1</strong>
-	</div>
-
 	<div class="form-group">
 		<label class="control-label col-sm-3"><?php echo __( 'Job agent' ); ?></label>
 		<div class="col-sm-3">
