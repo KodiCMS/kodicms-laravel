7a563c551fec848f1e2e4ec1e4fe0e14a4e67eb9
Butschster <butschster@gmail.com>
2015-05-16 21:37:39 +0300

    Небольшие изменения в тестовом шаблоне

--- resources/layouts/normal.blade.php
+++ resources/layouts/normal.blade.php
@@ -2,16 +2,13 @@
 <html lang="en">
 <head>
 	{!!
-		Meta::addPackage('jquery')
+		Meta::addPackage(['jquery'])
 			->setFavicon(resources_url() . '/favicon.ico')
 			->build()
 	!!}
 
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
