63ecff873ede8d0e6046ec062d97fe0507e0a3d7
Butschster <butschster@gmail.com>
2015-05-17 21:18:16 +0300

    fix

--- modules/Widgets/resources/views/widgets/page/iframe.blade.php
+++ modules/Widgets/resources/views/widgets/page/iframe.blade.php
@@ -3,6 +3,6 @@
 		@lang('widgets::core.title.list') <span class="text-muted">[{{ $page->getLayout() }}]</span>
 	</h4>
 </div>
-<div class="panel-body no-padding">
+<div class="panel-body no-padding panel-body-page-widgets">
 	<iframe src="{{ route('backend.pages.wysiwyg', [$page]) }}" style="width:100%;height:500px;border:none;"></iframe>
 </div>
