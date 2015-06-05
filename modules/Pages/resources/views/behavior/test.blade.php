bbefe2d3e770b94e88d57ed3120dbe3f95fe2d58
butschster <butschster@gmail.com>
2015-06-05 13:44:01 +0300

    Исправлена ошибка, возникающая при выборе поведения страницы при создании

--- modules/Pages/resources/views/behavior/test.blade.php
+++ modules/Pages/resources/views/behavior/test.blade.php
@@ -1,6 +1,10 @@
 <br />
-<label>{{ 'Document page' }}</label>
+@if($page)
+<label>Document page</label>
 {!! Form::select('behavior_settings[item_page_id]', $page->getSitemap(), $settings->item_page_id, [
 	'class' => 'form-control'
 ]) !!}
 <script>CMS.ui.init('select2')</script>
+@else
+<div class="alert alert-warning">Page not created</div>
+@endif
