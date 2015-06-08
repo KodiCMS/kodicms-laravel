b36b18396677d9cca5d22e82eae6b0873b739e55
Butschster <butschster@gmail.com>
2015-06-08 22:49:04 +0300

    Небольшой фикс дял поиска текущий страницы в админ панели

--- modules/CMS/Navigation/Section.php
+++ modules/CMS/Navigation/Section.php
@@ -143,7 +143,8 @@ class Section extends ItemDecorator implements \Countable, \Iterator
 			}
 
 			$uri = substr($currentUri, $len);
-			if (!empty($url) AND strpos($uri, $url) !== FALSE) {
+			$pos = strpos($uri, $url);
+			if (!empty($url) AND $pos !== false and $pos < 5) {
 				$page->setStatus(TRUE);
 
 				Collection::setCurrentPage($page);
