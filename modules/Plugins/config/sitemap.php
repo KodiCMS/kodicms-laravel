89f6b8f2bd48422e7c4d84a78cab708d6a3b59f6
Butschster <butschster@gmail.com>
2015-06-10 01:05:36 +0300

    Вынос раздела "плагины" в корень

--- modules/Plugins/config/sitemap.php
+++ modules/Plugins/config/sitemap.php
@@ -1,18 +1,11 @@
 <?php
 return [
 	[
-		'name' => 'System',
-
-		'children' => [
-			[
-				'name' => 'Plugins',
-				'divider' => true,
-				'label' => 'plugins::core.title',
-				'icon' => 'puzzle-piece',
-				'url' => route('backend.plugins.list'),
-				'priority' => 9999999,
-			]
-		]
-	],
-
+		'name' => 'Plugins',
+		'divider' => true,
+		'label' => 'plugins::core.title',
+		'icon' => 'puzzle-piece',
+		'url' => route('backend.plugins.list'),
+		'priority' => 9999999,
+	]
 ];
