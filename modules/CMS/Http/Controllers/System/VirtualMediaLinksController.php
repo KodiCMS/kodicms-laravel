224177190e1a1d3a18431ddf606e78eeb4f0d368
Butschster <butschster@gmail.com>
2015-07-04 01:40:28 +0300

    Убран лишний код

--- modules/CMS/Http/Controllers/System/VirtualMediaLinksController.php
+++ modules/CMS/Http/Controllers/System/VirtualMediaLinksController.php
@@ -10,23 +10,16 @@ class VirtualMediaLinksController extends Controller
 	{
 		$route = $this->getRouter()->getCurrentRoute();
 
-		// Get the file path from the request
 		$file = $route->getParameter('file');
 		$ext = $route->getParameter('ext');
 
-		// Remove the extension from the filename
 		if ($file = ModuleLoader::findFile('resources', $file, $ext))
 		{
-			// Check if the browser sent an "if-none-match: <etag>" header, and tell if the file hasn't changed
-
-			// Set the proper headers to allow caching
 			return (new Response(file_get_contents($file)))
 				->header('Content-Type', File::mimeByExt($ext))
 				->header('last-modified', date('r', filemtime($file)));
 		}
-		else
-		{
-			abort(404);
-		}
+
+		abort(404);
 	}
 }
