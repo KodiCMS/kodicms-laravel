9f90bba1386d5af8dd4f58eb7880eacdf4f7c2b7
Butschster <butschster@gmail.com>
2015-07-22 08:25:22 +0300

    Исправлен префикс маршрутов и минимизирован css

--- modules/Datasource/Http/routes.php
+++ modules/Datasource/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['prefix' => base_path(), 'as' => 'backend.datasource.'], function ()
+Route::group(['prefix' => backend_url(), 'as' => 'backend.datasource.'], function ()
 {
 	Route::get('datasource/{id}', [
 		'as' => 'list',
