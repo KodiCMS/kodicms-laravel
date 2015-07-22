bb81d1ae634f017fddc7f58c3fc9658446c6254f
Butschster <butschster@gmail.com>
2015-07-22 08:27:25 +0300

    Add section controller

---
+++ modules/Datasource/Http/Controllers/SectionController.php
@@ -0,0 +1,11 @@
+<?php namespace KodiCMS\Datasource\Http\Controllers;
+
+use KodiCMS\CMS\Http\Controllers\System\Controller;
+
+class SectionController extends Controller
+{
+	public function getCreate($type)
+	{
+
+	}
+}
