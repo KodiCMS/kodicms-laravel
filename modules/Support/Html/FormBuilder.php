e73e8924ea3a8274c17d9b4833c0f0c031fc8eac
Butschster <butschster@gmail.com>
2015-07-23 02:21:54 +0300

    Переопределен класс FormBuilder для добавления новых типов полей

---
+++ modules/Support/Html/FormBuilder.php
@@ -0,0 +1,25 @@
+<?php namespace KodiCMS\Support\Html;
+
+class FormBuilder extends \Illuminate\Html\FormBuilder
+{
+	/**
+	 * @param string $name
+	 * @param int $value
+	 * @param bool|null $checked
+	 * @param array $options
+	 * @return string
+	 */
+	public function switcher($name, $value = 1, $checked = null, $options = [])
+	{
+		$options = array_merge([
+			'class' => 'form-switcher',
+			'data-size' => 'small',
+			'data-width' => 60,
+			'data-on' => trans('cms::system.button.on'),
+			'data-off' => trans('cms::system.button.off'),
+			'data-onstyle' => 'success'
+		], $options);
+
+		return $this->checkbox($name, $value, $checked, $options);
+	}
+}
