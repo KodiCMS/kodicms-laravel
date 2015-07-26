7bf5a6961fa558916e9080111a98c6237ba30034
Butschster <butschster@gmail.com>
2015-07-26 23:30:25 +0300

    fix

--- modules/Datasource/resources/views/navigation.blade.php
+++ modules/Datasource/resources/views/navigation.blade.php
@@ -29,7 +29,7 @@
 	@if (!empty($sections))
 		<ul class="nav nav-pills nav-stacked">
 		@foreach ($sections as $section)
-			<li @if($currentSection->getId() == $section->getId()) class="active" @endif >
+			<li @if(!is_null($currentSection) and $currentSection->getId() == $section->getId()) class="active" @endif >
 				{!! HTML::link($section->getLink(), $section->name, [
 					'data-icon' => $section->getIcon()
 				]) !!}
