17e4dd33905c0ffd9069cd190d268658c942859c
Teodor Sandu <relu902@gmail.com>
2015-07-28 13:02:08 +0200

    datasource module English translation

---
+++ modules/Datasource/resources/lang/en/fields.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+	'settings' => [
+		'hint' => 'Hint'
+	],
+	'primary' => [
+		'title' => 'Primary'
+	],
+	'integer' => [
+		'title' => 'Number',
+		'length' => 'Length',
+		'min' => 'Minimum value',
+		'max' => 'Maximum value',
+		'auto_increment' => 'Auto increment',
+		'increment_step' => 'Step'
+	],
+	'string' => [
+		'title' => 'Line',
+		'use_filemanager' => 'Use file manager',
+		'length' => 'Length'
+	],
+	'email' => [
+		'title' => 'Email'
+	],
+	'slug' => [
+		'title' => 'Slug',
+		'is_unique' => 'Unique',
+		'from_document_title' => 'Take the value from the document header',
+		'separator' => 'Separator',
+		'must_be_unique' => 'The value of the field must be unique',
+	],
+	'textarea' => [
+		'title' => 'Теxt',
+		'allow_html' => 'Allow HTML tags',
+		'filter_html' => 'Filter HTML tag',
+		'allowed_tags' => 'Allowed tags',
+		'num_rows' => 'num_rows'
+	],
+	'html' => [
+		'title' => 'HTML editor',
+		'filter_html' => 'Filter HTML tag',
+		'allowed_tags' => 'Allowed tags',
+		'wysiwyg' => 'Text editor',
+		'remove_empty_tags' => 'Remove empty tags'
+	],
+	'boolean' => [
+		'title' => 'Boolean',
+		'style' => 'Style'
+	],
+	'timestamp' => [
+		'title' => 'Timestamp'
+	],
+	'date' => [
+		'title' => 'Date',
+		'set_current_date' => 'Set current date'
+	],
+	'datetime' => [
+		'title' => 'Date/Time'
+	],
+	'has_one' => [
+		'title' => 'HasOne'
+	],
+	'belongs_to' => [
+		'title' => 'Belongs to'
+	]
+];
---
+++ modules/Datasource/resources/lang/en/widgets.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'list' => [
+		'title' => 'List of documents'
+	]
+];
