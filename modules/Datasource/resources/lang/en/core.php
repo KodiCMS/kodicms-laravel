82a0a0d897db78c152bc57b509049a64b6df5d30
Teodor Sandu <relu902@gmail.com>
2015-08-14 14:17:56 +0200

    Datasource English translation

---
+++ modules/Datasource/resources/lang/en/core.php
@@ -0,0 +1,56 @@
+<?php
+return [
+	'title' => [
+		'information' => 'Information Sharing',
+		'section' => 'Sections',
+		'edit' => 'Edit section [:name]',
+		'create' => 'New section',
+		'create_document' => 'New document',
+		'edit_document' => 'Edit document [:name]'
+	],
+	'information' => [
+		'name' => 'Title',
+		'description' => 'Description',
+		'show_in_root' => 'Show in root menu',
+		'icon' => 'Icon',
+		'menu_position' => 'Menu position'
+	],
+	'ordering' => [
+		'title' => 'Sort documents',
+		'order_by' => 'Order by',
+		'fields' => 'Available fields'
+	],
+	'button' => [
+		'create' => 'Create partition',
+		'create_folder' => 'Create folder',
+		'remove_fields' => 'Remove fields',
+		'create_field' => 'Create field'
+	],
+	'toolbar' => [
+		'check_all' => 'All',
+		'uncheck_all' => 'None',
+		'remove_selected' => 'Remove selected',
+		'create_document' => 'Create document'
+	],
+	'field' => [
+		'information' => 'Information',
+		'list' => 'List of fields in this section',
+		'position' => 'Position',
+		'key' => 'Key',
+		'name' => 'Title',
+		'type' => 'Type',
+		'visible' => 'Show list',
+		'settings' => 'Settings',
+		'hint' => 'Hint',
+		'required' => 'Required',
+		'default_value' => 'Default value',
+		'section_id' => 'Section',
+		'related_section_id' => 'Related section',
+		'related' => 'Fields with links related to the current list',
+		'related_field_id' => 'Related field'
+	],
+	'messages' => [
+		'empty' => 'No documents',
+		'document_updated' => 'Document uploaded'
+	]
+];
---
+++ modules/Datasource/resources/lang/en/widgets.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+	'list' => [
+		'title' => 'List of documents',
+		'filtering' => [
+			'title' => 'Filtering conditions',
+			'query_string' => 'Advanced settings (in the format query string)',
+			'invert_condition' => 'Invert',
+			'condition_value' => 'Value',
+			'condition' => 'Condition',
+			'source' => 'Source',
+			'button_add' => 'Add Condition',
+			'where' => 'Where',
+			'field' => 'Field'
+		],
+		'settings' => [
+			'document_uri' => 'page document [URI]',
+			'example' => 'Example: [:text]',
+			'select_random_documents' => 'Select random documents',
+			'search_key' => 'The search key for documents  [$_GET]'
+		]
+	]
+];
