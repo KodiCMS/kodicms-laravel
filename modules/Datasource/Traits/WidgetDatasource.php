0d39cbadcf8ec09ffcb6f829eb4d29d7e024d40d
butschster <butschster@gmail.com>
2015-08-18 21:53:51 +0300

    Некоторые части виджета "Список документов" вынесены в traits

--- modules/Datasource/Traits/WidgetDatasource.php
+++ modules/Datasource/Traits/WidgetDatasource.php
@@ -17,4 +17,17 @@ trait WidgetDatasource
 	{
 		return (int) $this->section_id;
 	}
+
+	/**
+	 * @return \Illuminate\Database\Eloquent\Model|SectionInterface|null
+	 */
+	public function getSection()
+	{
+		if (is_null($this->section) and $this->isDatasourceSelected())
+		{
+			$this->section = $this->sectionRepository->findOrFail($this->getSectionId());
+		}
+
+		return $this->section;
+	}
 }
