e822792916b10c0b3e6614c3301a5d10460c32c0
butschster <butschster@gmail.com>
2015-08-24 10:11:42 +0300

    Исправлена ошибка из за которой не работало создание полей в разделе Datasource

--- modules/Datasource/resources/views/field/types/image.blade.php
+++ modules/Datasource/resources/views/field/types/image.blade.php
@@ -1,16 +1,15 @@
-	<div class="form-group form-inline">
-		<label class="control-label col-md-3">@lang('datasource::fields.image.max_file_size')</label>
-		<div class="col-md-9">
-			{!! Form::text('settings[max_file_size]', $field->getMaxFileSize(), [
-				'class' => 'form-control', 'id' => 'max_file_size', 'size' => 10
-			]) !!}
-			<span class="flags">
-				<span class="label" data-value="{{ \KodiCMS\Support\Helpers\Num::bytes('100K') }}">100k</span>
-				<span class="label" data-value="{{ \KodiCMS\Support\Helpers\Num::bytes('1MiB') }}">1Mib</span>
-				<span class="label" data-value="{{ \KodiCMS\Support\Helpers\Num::bytes('5MiB') }}">5Mib</span>
-				<span class="label" data-value="{{ \KodiCMS\Support\Helpers\Num::bytes('10MiB') }}">10Mib</span>
-			</span>
-		</div>
+<div class="form-group form-inline">
+	<label class="control-label col-md-3">@lang('datasource::fields.image.max_file_size')</label>
+	<div class="col-md-9">
+		{!! Form::text('settings[max_file_size]', $field->getMaxFileSize(), [
+			'class' => 'form-control', 'id' => 'max_file_size', 'size' => 10
+		]) !!}
+		<span class="flags">
+			<span class="label" data-value="{{ \KodiCMS\Support\Helpers\Num::bytes('100K') }}">100k</span>
+			<span class="label" data-value="{{ \KodiCMS\Support\Helpers\Num::bytes('1MiB') }}">1Mib</span>
+			<span class="label" data-value="{{ \KodiCMS\Support\Helpers\Num::bytes('5MiB') }}">5Mib</span>
+			<span class="label" data-value="{{ \KodiCMS\Support\Helpers\Num::bytes('10MiB') }}">10Mib</span>
+		</span>
 	</div>
 </div>
 <div class="panel-heading">
@@ -62,7 +61,7 @@
 		</div>
 	</div>
 </div>
-
+@if($field->exists)
 <div class="panel-heading">
 	<span class="panel-title">@lang('datasource::fields.image.same_image_fields')</span>
 </div>
@@ -72,4 +71,5 @@
 		'multiple', 'class' => 'form-control'
 	]) !!}
 </div>
-<div class="panel-body">
+
+@endif
