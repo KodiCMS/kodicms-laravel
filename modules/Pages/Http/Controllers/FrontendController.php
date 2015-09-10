561016b03453f1042b6bd862791f12de171a5ad9
Butschster <butschster@gmail.com>
2015-09-10 17:26:45 +0300

    Ипсравление ошибки из за которой выводилось неверно сообщение при отсутсвии шаблона у страницы

--- modules/Pages/Http/Controllers/FrontendController.php
+++ modules/Pages/Http/Controllers/FrontendController.php
@@ -1,4 +1,6 @@
-<?php namespace KodiCMS\Pages\Http\Controllers;
+<?php
+
+namespace KodiCMS\Pages\Http\Controllers;
 
 use KodiCMS\Pages\Model\FrontendPage;
 use KodiCMS\Pages\Exceptions\PageNotFoundException;
@@ -8,47 +10,36 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class FrontendController extends FrontPageController
 {
-	public function run($slug)
-	{
-		event('frontend.requested', [$slug]);
 
-		$frontPage = FrontendPage::findByUri($slug);
-		$notFoundMessage = trans('pages::core.messages.not_found');
+    public function run($slug)
+    {
+        event('frontend.requested', [ $slug ]);
 
-		if ($frontPage instanceof FrontendPage)
-		{
-			if ($frontPage->isRedirect() AND strlen($frontPage->getRedirectUrl()) > 0)
-			{
-				return redirect($frontPage->getRedirectUrl(), 301);
-			}
-			else
-			{
-				try
-				{
-					event('frontend.found', [$frontPage]);
+        $frontPage       = FrontendPage::findByUri($slug);
+        $notFoundMessage = trans('pages::core.messages.not_found');
 
-					$layout = $frontPage->getLayoutView();
+        if ($frontPage instanceof FrontendPage) {
+            if ($frontPage->isRedirect() AND strlen($frontPage->getRedirectUrl()) > 0) {
+                return redirect($frontPage->getRedirectUrl(), 301);
+            } else {
+                try {
+                    event('frontend.found', [ $frontPage ]);
 
-					return $this->render($layout, $frontPage->getMime());
-				}
-				catch (NotFoundHttpException $e)
-				{
-					$notFoundMessage = $e->getMessage();
-				}
-				catch(HttpException $e)
-				{
-					$notFoundMessage = $e->getMessage();
-				}
+                    return $this->render($frontPage->getLayoutView(), $frontPage->getMime());
+                } catch (NotFoundHttpException $e) {
+                    $notFoundMessage = $e->getMessage();
+                } catch (HttpException $e) {
+                    $notFoundMessage = $e->getMessage();
+                }
 
-			}
-		}
+            }
+        }
 
-		if (config('cms.find_similar') AND ($uri = FrontendPage::findSimilar($slug)) !== false)
-		{
-			return redirect($uri, 301);
-		}
+        if (config('cms.find_similar') AND ( $uri = FrontendPage::findSimilar($slug) ) !== false) {
+            return redirect($uri, 301);
+        }
 
-		event('frontend.not_found', [$slug]);
-		throw new PageNotFoundException($notFoundMessage);
-	}
+        event('frontend.not_found', [ $slug ]);
+        throw new PageNotFoundException($notFoundMessage);
+    }
 }
